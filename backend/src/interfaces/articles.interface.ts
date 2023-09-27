6d0310f8733567327918eb66140f62f6f0ef29b8
TheMysterys <the-mystery@outlook.com>
2023-09-27 17:47:54 +1300

    Articles interface

---
+++ backend/src/interfaces/articles.interface.ts
@@ -0,0 +1,11 @@
+import { Document } from "mongoose";
+
+export interface Article extends Document {
+  title: string;
+  authors: string;
+  source: string;
+  pubYear: string;
+  doi: string;
+  summary: string;
+  submitDate: Date;
+}
