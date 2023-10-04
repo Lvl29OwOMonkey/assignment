5473157c2210a41ba6d6bfe034de4c7e23c9c9a7
TheMysterys <the-mystery@outlook.com>
2023-10-04 13:41:45 +1300

    Add header

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div className="container">
+      <Head>
+				<title>SPEED</title>
+			</Head>
       <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
     </div>
   );
