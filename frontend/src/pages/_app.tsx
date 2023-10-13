2ddfca2c15e945d95e224bbc2536a63187c144bd
TheMysterys <the-mystery@outlook.com>
2023-10-13 15:03:15 +1300

    Formatting changes

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,12 +4,12 @@ import { SessionProvider } from "next-auth/react";
 import PopulatedNavBar from "../components/PopulatedNavBar";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <PopulatedNavBar />
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+	return (
+		<SessionProvider session={session}>
+			<PopulatedNavBar />
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 }
 
 export default MyApp;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
+	return (
+		<div className="container">
+			<Head>
 				<title>SPEED</title>
 			</Head>
-      <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
-    </div>
-  );
+			<h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+		</div>
+	);
 }
