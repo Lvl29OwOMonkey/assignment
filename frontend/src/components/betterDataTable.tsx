12c17d5e8b4560352a03def9a32d8758457329c0
TheMysterys <the-mystery@outlook.com>
2023-10-18 14:41:13 +1300

    Better table use input for columns

--- frontend/src/components/betterDataTable.tsx
+++ frontend/src/components/betterDataTable.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
-const columns: GridColDef[] = [
-	{ field: "title", headerName: "Title" },
-	{ field: "authors", headerName: "Authors" },
-	{ field: "source", headerName: "Source" },
-	{ field: "pubYear", headerName: "Publication Year" },
-	{ field: "doi", headerName: "DOI" },
-	{ field: "claim", headerName: "Claim" },
-	{ field: "evidence", headerName: "Evidence" },
-];
-
 interface DataTableProps {
+	columns: GridColDef[];
 	rows: any[]; // Change the type to match your data structure
 }
 
-const BetterDataTable: React.FC<DataTableProps> = ({ rows }) => {
+const BetterDataTable: React.FC<DataTableProps> = ({ columns, rows }) => {
 	columns.forEach((column) => {
 		column.flex = 1;
 	});
@@ -42,28 +33,6 @@ const BetterDataTable: React.FC<DataTableProps> = ({ rows }) => {
 					menuIconButton: "text-white",
 				}}
 			/>
-			{/* <TableHead>
-				<TableRow>
-					{columns.map((column) => (
-						<TableCell
-							key={column.field}
-							classes={{ root: "text-white font-bold" }}
-						>
-							<TableSortLabel
-								active={true}
-								direction="asc"
-								sx={{
-									"& .MuiTableSortLabel-icon": { color: "white !important" },
-                  "& .MuiTableSortLabel-root": { color: "white !important" },
-								}}
-								// onClick={createSortHandler(column.field)}
-							>
-								{column.headerName}
-							</TableSortLabel>
-						</TableCell>
-					))}
-				</TableRow>
-			</TableHead> */}
 		</div>
 	);
 };
