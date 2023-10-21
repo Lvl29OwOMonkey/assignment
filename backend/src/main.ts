e4363ff5908c4c9b53fcb8fabc0b6fe530c77f82
TheMysterys <the-mystery@outlook.com>
2023-10-21 21:37:12 +1300

    Vercel I hate you

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,7 @@ import { configDotenv } from "dotenv";
 async function bootstrap() {
   configDotenv();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
-  });
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
